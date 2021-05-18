9eeb1821903b622fcae026d12f7725951e5122ed
JosephSimmons <josephsimmons@outlook.com>
2021-05-18 23:57:09 +0100

    ColorPalette component now reusable

--- screens/ColorPalette.js
+++ screens/ColorPalette.js
@@ -1,27 +1,9 @@
 import React from 'react';
-import { View, Text, SafeAreaView, StyleSheet, FlatList } from 'react-native';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
 import ColorBox from '../components/ColorBox';
 
-const ColorPalette = () => {
-  const COLORS = [
-    { colorName: 'Base03', hexCode: '#002b36' },
-    { colorName: 'Base02', hexCode: '#073642' },
-    { colorName: 'Base01', hexCode: '#586e75' },
-    { colorName: 'Base00', hexCode: '#657b83' },
-    { colorName: 'Base0', hexCode: '#839496' },
-    { colorName: 'Base1', hexCode: '#93a1a1' },
-    { colorName: 'Base2', hexCode: '#eee8d5' },
-    { colorName: 'Base3', hexCode: '#fdf6e3' },
-    { colorName: 'Yellow', hexCode: '#b58900' },
-    { colorName: 'Orange', hexCode: '#cb4b16' },
-    { colorName: 'Red', hexCode: '#dc322f' },
-    { colorName: 'Magenta', hexCode: '#d33682' },
-    { colorName: 'Violet', hexCode: '#6c71c4' },
-    { colorName: 'Blue', hexCode: '#268bd2' },
-    { colorName: 'Cyan', hexCode: '#2aa198' },
-    { colorName: 'Green', hexCode: '#859900' },
-  ];
-
+const ColorPalette = ({ route }) => {
+  const { paletteName, colors } = route.params;
   return (
     <View style={styles.safeAreaContainer}>
       <View style={styles.header}>
@@ -31,13 +13,13 @@ const ColorPalette = () => {
       </View>
       <FlatList
         style={styles.listPadding}
-        data={COLORS}
+        data={colors}
         keyExtractor={(item) => item.hexCode}
         renderItem={({ item }) => (
           <ColorBox colorName={item.colorName} hexCode={item.hexCode} />
         )}
         ListHeaderComponent={
-          <Text style={[styles.header, styles.boldtxt]}>Solarized</Text>
+          <Text style={[styles.header, styles.boldtxt]}>{paletteName}</Text>
         }
       />
     </View>
